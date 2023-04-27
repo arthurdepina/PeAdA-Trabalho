c604d17947db3e6d3a8718d26ea502f3d7d2998c
Arthur <84559827+arthurdepina@users.noreply.github.com>
2023-04-26 22:14:41 -0300

    Leitura do arquivo.
    Até agora o programa lê o arquivo e coloca as palavras em um vetor de chars.
    Ainda estou pensando em um jeito de verificar se uma palavra já está no vetor, para que não seja inserida novamente.

--- GeraDicionario.c
+++ GeraDicionario.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+#define MAX_LEN 50
+
+int busca_dicionario(char **vetor_palavras, char *palavra){
+    for(int i = 0; i < 100; i++){
+        if(vetor_palavras[i] == NULL){
+            break;
+        }
+    }
+    return 1;
+}
 
 int main(){
+    FILE *livro;
+    char temp[MAX_LEN];
+    int count = 0;
+
+    livro = fopen("texto.txt", "r");
+    if (livro == NULL){
+        printf("Erro");
+        exit(1);
+    }
+
+    char **palavras = malloc(100 * sizeof(char*));
+    while(fscanf(livro, "%s", temp) != EOF && count < 100){
+        if (strcmp(temp, ".") != 0) {
+            for(int i = 0; i < strlen(temp); i++){
+                temp[i] = tolower(temp[i]);
+            }
+            if(busca_dicionario(palavras, temp)){
+                palavras[count] = malloc((strlen(temp)+1) * sizeof(char));
+                strcpy(palavras[count], temp);
+                count++;
+            }else{
+                continue;
+            }
+        }
+    }
+
+    for(int i = 0; i < count; i++){
+        printf("%s\n", palavras[i]);
+    }
+
+    for(int i = 0; i < count; i++){
+        free(palavras[i]);
+    }
+    fclose(livro);
+    free(palavras);
     return 0;
 }

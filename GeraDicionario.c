100207efc4117de9487995b5d883272807e3461b
Arthur <84559827+arthurdepina@users.noreply.github.com>
2023-05-01 22:14:07 -0300

    Finalizando :+1:
    Adicionando Comentários

--- GeraDicionario.c
+++ GeraDicionario.c
@@ -1,9 +1,88 @@
+// Arthur de Pina Balduino Leitão - 32207840
+// Cesar Hideki Imai - 32255659
+
+/*                        
+                           Explicação Geral:
+
+    O programa começa abrindo o arquivo, que recebe o nome "livro".
+    Se livro retornar NULL, uma mensagem de erro é impressa e o
+    programa é finalizado. 
+    
+    Em seguida é alocado uma lista para armazenar as palavras do livro,
+    a lista "palavras". O tamanho é 100, pois o enunciado especifica
+    que não haverão mais de 100 palavras na lista.
+    
+    O loop while coloca as palavras do texto na lista "palavras". Para
+    isso, itera-se sobre as palavras do arquivo e a palavra atual é
+    chamada de "temp". "temp" é comparada com ".", para saber se o
+    arquivo chegou ao fim. Em seguida, todas as letras da palavra são
+    transformadas em minúsculas na função "para_minusculas". Então, é
+    utilizada a função "busca_dicionario" para verificar se a palavra
+    já consta na lista "palavras". Se a palavra ainda não consta em
+    "palavras", é chamada a função "insere_palavra", que insere a palavra
+    na lista em ordem alfabética decrescente. Se a palavra já está na
+    lista, ela não é adicionada e o programa passa para a próxima palavra
+    (se houver).
+    
+    Terminado o loop, temos uma lista com todas as palavras do texto
+    em ordem alfabética decrescente. Então criamos (ou abrimos) o arquivo
+    "dicionario.txt" na variável "dicionario" e escrevemos cada palavra
+    da lista "palavra" em cada linha de "dicionario.txt". Escrevemos também
+    uma última linha com o número de palavras do dicionario.
+    
+    Feito isso, libera-se o espaço utilizado por "palavras", e fechamos
+    os arquivos "livro" e "palavra".
+
+
+                        Explicação das Funções:
+
+    para_minusculas --> A função recebe uma string e faz um loop pelos
+    caracteres, usando o tolower() do ctype.h para transformar cada um
+    em letra minúscula.
+
+    busca_dicionario --> A função recebe uma lista de palavra, uma
+    palavra, a posição do início da lista e posição do final da lista
+    e realiza busca binária de forma recursiva para verificar se uma
+    palavra já consta na lista, retornando 0 se a palavra já consta e 1
+    se a palavra não consta. Inicialmente, a função verifica se inicio
+    é maior que fim, e se for, a palavra não foi encontrada e retorna-se 1.
+    Caso contrário, a função calcula o índice do elemento do meio e
+    compara a palavra passada com o elemento do meio. Se a palavra for
+    encontrada, a função retorna 0. Se a palavra for menor que o elemento
+    central, a função chama a si mesma com os parâmetros: vetor de palavras,
+    palavra buscada, índice inicial e índice anterior ao do elemento do
+    meio. Se a palavra for maior que o elemento central, a função chama a
+    si mesma com parâmetros: vetor de palavras, palavra buscada, índice do
+    elemento seguinte ao elemento do meio e índice final. 
+
+    insere_palavra --> A função é recursiva e recebe uma lista de palavras,
+    uma palavra, uma posição e o número de palavras na lista. A função
+    primeiro verifica se a palavra pode ser inserida na primeira posição
+    da lista e, se for possível, insere. Se não for possível a função
+    compara a palavra a ser inserida, com a palavra que já consta na posição
+    passada como parâmetro. Se a palavra a ser inserida for maior
+    lexicograficamente que a palavra que já consta, todas as palavras a
+    partir da posição passada são deslocadas para direita a fim de inserir
+    a palavra desejada de modo que função fique em ordem alfabética
+    descrescente. Se a palavra a ser inserida não for maior lexicograficamente,
+    a função se chama recursivamente com a posição incrementada em um. Assim,
+    a função avança no vetor até encontrar a posição adequada para inserir
+    a palavra. A função utiliza strdup() da string.h que aloca memória para
+    string e copia a string para o espaço de memória alocado.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 #define MAX_LEN 50
+
+void para_minusculas(char *str) {
+    for(int i = 0; str[i]; i++) {
+        str[i] = tolower(str[i]);
+    }
+}
 
 int busca_dicionario(char **vetor_palavras, char *palavra, int inicio, int fim){
     if (inicio > fim) {
@@ -19,18 +98,12 @@ int busca_dicionario(char **vetor_palavras, char *palavra, int inicio, int fim){
     }
 }
 
-void paraMinusculas(char *str) {
-    for(int i = 0; str[i]; i++) {
-        str[i] = tolower(str[i]);
-    }
-}
-
 void insere_palavra(char **palavras, char *palavra, int pos, int count) {
     if (pos == count) {
         palavras[count] = strdup(palavra);
         return;
     }
-    if (strcmp(palavra, palavras[pos]) > 0) { // Alterada a condição aqui
+    if (strcmp(palavra, palavras[pos]) > 0) {
         for (int i = count; i > pos; i--) {
             palavras[i] = palavras[i - 1];
         }
@@ -54,7 +127,7 @@ int main(){
     char **palavras = malloc(100 * sizeof(char*));
     while(fscanf(livro, "%s", temp) != EOF && count < 100){
         if (strcmp(temp, ".") != 0) {
-            paraMinusculas(temp);
+            para_minusculas(temp);
             if(busca_dicionario(palavras, temp, 0, count-1)){ // Se a palavra não foi encontrada
                 insere_palavra(palavras, temp, 0, count);
                 count++;
@@ -70,12 +143,6 @@ int main(){
         fprintf(dicionario, "%s\n", palavras[i]);
     }
     fprintf(dicionario, "%s%d\n", ultima_linha, count);
-
-    /*
-    for(int i = 0; i < count; i++){
-        printf("%s\n", palavras[i]);
-    }
-    */
 
     for(int i = 0; i < count; i++){
         free(palavras[i]);
